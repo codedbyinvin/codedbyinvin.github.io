bf1f0ba88a4a0e2165479fc85d97db258dc86add
codedbyinvin <100071469+codedbyinvin@users.noreply.github.com>
2023-11-11 07:55:25 +0100

    made counter page

--- js/script.js
+++ js/script.js
@@ -1,21 +1,88 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Date cible (format: année, mois - 1, jour)
-    const targetDate = new Date(2023, 0, 1);
+document.addEventListener("DOMContentLoaded", () => {
+    class Counter {
+        constructor(counterId, resetId, storageKey) {
+            this.counterElement = document.getElementById(counterId);
+            this.resetButton = document.getElementById(resetId);
+            this.storageKey = storageKey;
+            this.today = new Date();
+            this.firstVisit = this.getFirstVisit();
+            this.updateCounter();
+            this.resetButton.addEventListener("click", () => this.resetCounter());
+        }
 
-    function updateCountdown() {
-        const currentDate = new Date();
-        const difference = targetDate - currentDate;
+        getFirstVisit() {
+            let firstVisit = new Date(parseInt(localStorage.getItem(this.storageKey)));
+            if (!firstVisit || isNaN(firstVisit.getTime())) {
+                firstVisit = this.today;
+                localStorage.setItem(this.storageKey, firstVisit.getTime());
+            }
+            return firstVisit;
+        }
 
-        // Calcul du nombre de jours restants
-        const daysRemaining = Math.floor(difference / (1000 * 60 * 60 * 24));
+        getTimeSince() {
+            this.today = new Date();
+            let difference = this.today.getTime() - this.firstVisit.getTime();
+        
+            const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+            difference -= days * 1000 * 60 * 60 * 24;
+        
+            const hours = Math.floor(difference / (1000 * 60 * 60));
+            difference -= hours * 1000 * 60 * 60;
+        
+            const minutes = Math.floor(difference / (1000 * 60));
+            difference -= minutes * 1000 * 60;
+        
+            const seconds = Math.floor(difference / 1000);
+        
+            return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+        }
+        updateCounter() {
+            this.counterElement.innerHTML = this.getTimeSince();
+        }
 
-        // Mettre à jour l'élément HTML du compteur
-        document.getElementById("countdown").innerHTML = `${daysRemaining} jours restants`;
+        resetCounter() {
+            this.firstVisit = new Date();
+            localStorage.setItem(this.storageKey, this.firstVisit.getTime());
+            this.updateCounter();
+        }
     }
 
-    // Mettre à jour le compteur toutes les secondes
-    setInterval(updateCountdown, 1000);
+        new Counter("counter_moine", "reset_moine", "firstVisit_ill");
+        new Counter("counter_fit", "reset_fit", "firstVisit_fit");
+        new Counter("counter_eat", "reset_eat", "firstVisit_eat");
 
-    // Mettre à jour le compteur une fois au chargement de la page
-    updateCountdown();
+        var canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d');
+        var W = window.innerWidth;
+        var H = window.innerHeight;
+
+        canvas.width = W;
+        canvas.height = H;
+
+        var fontSize = 16;
+        var columns = Math.floor(W / fontSize);
+        var drops = [];
+        for(var i=0; i<columns; i++){
+            drops.push(0);
+        }
+        var str = "JavaScript Hacking Effect";
+        function draw(){
+            context.fillStyle = "rgba(0,0,0,0.05)";
+            context.fillRect(0, 0, W, H);
+            context.fontSize = "700 " + fontSize + "px";
+            context.fillStyle = "#00cc33";
+            for(var i=0; i<columns; i++){
+                var index = Math.floor(Math.random()*str.length);
+                var x = i * fontSize;
+                var y = drops[i] * fontSize;
+                context.fillText(str[index], x, y);
+                if(y >= canvas.height && Math.random() > 0.99){
+                    drops[i] = 0;
+                }
+                drops[i]++;
+            }
+        }
+        draw();
+        setInterval(draw, 35);
+
 });
